40a82bc0b92af9c8d27626f690938cb8f67e81be
CaitCat <caitlynrosestewart@gmail.com>
2021-08-31 18:07:12 +0100

    got the keyboard to stop printing key released codes and special key presses. Need to work on a few bugs still

--- src/inc/interupts.asm
+++ src/inc/interupts.asm
@@ -35,21 +35,31 @@ idt_info:
 isr1: 
     in al, 0x64 ;read in byte from keyboard status register
     test al, 0x01 ;test if buffer full bit is set 
-    jz keyboard_locked ;if not set don't proced 
+    jz skip_printing ;if not set don't proced 
     test al, 0x20 ;keyboard locked bit set 
-    jnz keyboard_locked ;if it is don't proced
+    jnz skip_printing ;if it is don't proced
 
     in al,0x60 ;read in scan code 
- 
-    cmp al, 0x3a
-    jl keyboard_print 
-    jmp keyboard_locked 
+    push rax ;save scan code 
+    and al, 0x80 ;and the 8 bit as this should only be set on released characters 
+    cmp al, 0x80 ;check if the scan code now = 80 
+    pop rax ;restore code 
+    je skip_printing 
+    
     keyboard_print:
+    push rbx ;save video memory location
+    xor rbx,rbx ;clean out high bytes of rbx
+    mov bl,al ;mov scan code into bl
+    mov al, [keyset1+rbx] ;look up the ascii value in the scan code table 
+    pop rbx ;restore video memory 
+    
+    cmp al,0x00 ;check if the key is speacial key ctrl, alt, tab, etc...
+    je skip_printing ;if it is don't print it 
 
-    mov [rbx],al
-    add rbx,2
+    mov [rbx],al ;mov character into video memory if it's not special char 
+    add rbx,2 ;add 2 to the video memory value 
 
-    keyboard_locked:
+    skip_printing:
     ;send end of interupt to the PIC so it knows a new interupt may be processed
     mov al,0x20
     out PIC1_Com,al
@@ -64,5 +74,6 @@ load_idt:
     sti
     ret
 
-keyset1: db 0x00,0x00,'1234567890-=',0x00,0x00,'qwertyuiop[]',0,0,"asdfghjkl;'`",0x00,'\zxcvbnm,./',0x00,'*',0x00,' '
+keyset1: db 0x00,0x00,'1234567890-=',0x00,0x00,'qwertyuiop[]',0x00,0x00,"asdfghjkl;'`",0x00,'\zxcvbnm,./',0x00,'*',0x00,' ',0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'789-456+1230.',0x00,0x00,0x00,0x00,0x00,0x00
+
 
